828b7be91d5b3c9994036017cd911115fb56d711
Yiqin Zhou <yiqin.zhou@duke.edu>
2017-07-19 15:48:16 +0200

    Chat part worked

--- ChatChannelTableViewController.swift
+++ ChatChannelTableViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+
+enum Section: Int {
+    case createNewChannelSection = 0
+    case currentChannelsSection
+}
 
 class ChatChannelTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
+    
+    var senderDisplayName: String? // 1
+    var newChannelTextField: UITextField? // 2
+    private var channels: [Channel] = [] // 3
+    private lazy var channelRef: DatabaseReference = Database.database().reference().child("channels")
+    private var channelRefHandle: DatabaseHandle?
+    
+    
+  
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -29,23 +35,102 @@ class ChatChannelTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { // 2
+        if let currentSection: Section = Section(rawValue: section) {
+            switch currentSection {
+            case .createNewChannelSection:
+                return 1
+            case .currentChannelsSection:
+                return channels.count
+            }
+        } else {
+            return 0
+        }
     }
-
-    /*
+    
+    // 3
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let reuseIdentifier = (indexPath as NSIndexPath).section == Section.createNewChannelSection.rawValue ? "NewChannel" : "ExistingChannel"
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        
+        if (indexPath as NSIndexPath).section == Section.createNewChannelSection.rawValue {
+            if let createNewChannelCell = cell as? CreateChannelCell {
+                newChannelTextField = createNewChannelCell.newChannelNameField
+            }
+        } else if (indexPath as NSIndexPath).section == Section.currentChannelsSection.rawValue {
+            cell.textLabel?.text = channels[(indexPath as NSIndexPath).row].name
+        }
+        
         return cell
     }
-    */
+    
+    private func observeChannels() {
+        // Use the observe method to listen for new
+        // channels being written to the Firebase DB
+        channelRefHandle = channelRef.observe(.childAdded, with: { (snapshot) -> Void in // 1
+            let channelData = snapshot.value as! Dictionary<String, AnyObject> // 2
+            let id = snapshot.key
+            if let name = channelData["name"] as! String!, name.characters.count > 0 { // 3
+                self.channels.append(Channel(id: id, name: name))
+                self.tableView.reloadData()
+            } else {
+                print("Error! Could not decode channel data")
+            }
+        })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Chat"
+        observeChannels()
+     
+  
+    }
+    
+    deinit {
+        if let refHandle = channelRefHandle {
+            channelRef.removeObserver(withHandle: refHandle)
+        }
+    }
+ 
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == Section.currentChannelsSection.rawValue {
+            let channel = channels[(indexPath as NSIndexPath).row]
+            self.performSegue(withIdentifier: "ShowChannel", sender: channel)
+        }
+    }
+    
+ 
+ 
+    @IBAction func CreateChannel(_ sender: Any) {
+        if let name = newChannelTextField?.text { // 1
+            let newChannelRef = channelRef.childByAutoId() // 2
+            let channelItem = [ // 3
+                "name": name
+            ]
+            newChannelRef.setValue(channelItem) // 4
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+     
+        
+        
+        if let channel = sender as? Channel {
+            let chatVc = segue.destination as! ChatPortViewController
+            senderDisplayName = Auth.auth().currentUser?.email
+            
+            chatVc.senderDisplayName = senderDisplayName
+            chatVc.channel = channel
+            chatVc.channelRef = channelRef.child(channel.id)
+        }
+    }
 
     /*
     // Override to support conditional editing of the table view.
